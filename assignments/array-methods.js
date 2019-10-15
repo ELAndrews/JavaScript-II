635de29ecf756d2176086c0af3b202286f3b1431
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-10-15 13:53:23 +0100

    Add challenge 5 array-methods.js assignment answer

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -71,21 +71,40 @@ console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
-let runnersLargeSizeShirt = runners.filter((curr) => {return curr.shirt_size === "L"});
+const runnersLargeSizeShirt = runners.filter((curr) => {return curr.shirt_size === "L"});
 
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = runners.reduce((acc, curr) => {return acc + curr.donation}, 0);
+const ticketPriceTotal = runners.reduce((acc, curr) => {return acc + curr.donation}, 0);
 
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 - A list of runners who have collected more than 200 donations
 
-// Problem 2
+const bigDonations = runners.filter((curr) => { return curr.donation >= 200});
 
-// Problem 3
+console.log(bigDonations);
+
+// Problem 2 - company Skinix
+
+const skinixEmployees = runners.filter((curr) => { return curr.company_name === "Skinix"});
+
+console.log(skinixEmployees);
+
+// Problem 3 - college students 
+
+const collegeStudents = [];
+
+runners.forEach((curr) => {
+  if (curr.email.includes(".edu")) {
+    collegeStudents.push(curr);
+  }
+})
+
+
+console.log(collegeStudents);
